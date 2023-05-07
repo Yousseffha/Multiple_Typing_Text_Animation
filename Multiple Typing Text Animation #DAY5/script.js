1da23fbb14a5408ba0b84adbd7ae7beec82e2970
youssef hamdi <103889591+Yousseffha@users.noreply.github.com>
2023-05-07 09:40:46 +0100

    Add files via upload

---
+++ Multiple Typing Text Animation #DAY5/script.js
@@ -0,0 +1,360 @@
+let text = document.getElementById("text");
+
+//blogger
+
+setTimeout(function () {
+  text.append("b");
+}, 100);
+setTimeout(function () {
+  text.append("l");
+}, 200);
+setTimeout(function () {
+  text.append("o");
+}, 300);
+setTimeout(function () {
+  text.append("g");
+}, 400);
+setTimeout(function () {
+  text.append("g");
+}, 500);
+setTimeout(function () {
+  text.append("e");
+}, 600);
+setTimeout(function () {
+  text.append("r");
+  text.style.paddingRight = "5px"
+}, 700);
+
+setTimeout(function () {
+  setTimeout(function () {
+    text.style.padding = "0"
+    text.innerHTML = "blogge";
+  }, 100);
+  setTimeout(function () {
+    text.innerHTML = "blogg";
+  }, 200);
+  setTimeout(function () {
+    text.innerHTML = "blog";
+  }, 300);
+  setTimeout(function () {
+    text.innerHTML = "blo";
+  }, 400);
+  setTimeout(function () {
+    text.innerHTML = "bl";
+  }, 500);
+  setTimeout(function () {
+    text.innerHTML = "b";
+  }, 600);
+  setTimeout(function () {
+    text.innerHTML = "";
+  }, 700);
+}, 1000);
+
+//youtuber
+
+setTimeout(function () {
+  setTimeout(function () {
+    text.append("y");
+  }, 100);
+  setTimeout(function () {
+    text.append("o");
+  }, 200);
+  setTimeout(function () {
+    text.append("u");
+  }, 300);
+  setTimeout(function () {
+    text.append("t");
+  }, 400);
+  setTimeout(function () {
+    text.append("u");
+  }, 500);
+  setTimeout(function () {
+    text.append("b");
+  }, 600);
+  setTimeout(function () {
+    text.append("e");
+  }, 700);
+  setTimeout(function () {
+    text.append("r");
+    text.style.paddingRight = "5px"
+  }, 700);
+}, 2000);
+
+setTimeout(function () {
+  setTimeout(function () {
+    text.style.padding = "0"
+    text.innerHTML = "youtube";
+  }, 100);
+  setTimeout(function () {
+    text.innerHTML = "youtub";
+  }, 200);
+  setTimeout(function () {
+    text.innerHTML = "youtu";
+  }, 300);
+  setTimeout(function () {
+    text.innerHTML = "yout";
+  }, 400);
+  setTimeout(function () {
+    text.innerHTML = "you";
+  }, 500);
+  setTimeout(function () {
+    text.innerHTML = "yo";
+  }, 600);
+  setTimeout(function () {
+    text.innerHTML = "y";
+  }, 700);
+  setTimeout(function () {
+    text.innerHTML = "";
+  }, 800);
+}, 3000);
+
+// freelancer
+
+setTimeout(function () {
+  setTimeout(function () {
+    text.append("f");
+  }, 100);
+  setTimeout(function () {
+    text.append("r");
+  }, 200);
+  setTimeout(function () {
+    text.append("e");
+  }, 300);
+  setTimeout(function () {
+    text.append("e");
+  }, 400);
+  setTimeout(function () {
+    text.append("l");
+  }, 500);
+  setTimeout(function () {
+    text.append("a");
+  }, 600);
+  setTimeout(function () {
+    text.append("n");
+  }, 700);
+  setTimeout(function () {
+    text.append("c");
+  }, 800);
+  setTimeout(function () {
+    text.append("e");
+  }, 900);
+  setTimeout(function () {
+    text.append("r");
+    text.style.paddingRight = "5px"
+  }, 1000);
+}, 4100);
+
+setTimeout(function () {
+  setTimeout(function () {
+    text.style.padding = "0"
+    text.innerHTML = "freelance";
+  }, 100);
+  setTimeout(function () {
+    text.innerHTML = "freelanc";
+  }, 200);
+  setTimeout(function () {
+    text.innerHTML = "freelan";
+  }, 300);
+  setTimeout(function () {
+    text.innerHTML = "freela";
+  }, 400);
+  setTimeout(function () {
+    text.innerHTML = "freel";
+  }, 500);
+  setTimeout(function () {
+    text.innerHTML = "free";
+  }, 600);
+  setTimeout(function () {
+    text.innerHTML = "fre";
+  }, 700);
+  setTimeout(function () {
+    text.innerHTML = "fr";
+  }, 800);
+  setTimeout(function () {
+    text.innerHTML = "f";
+  }, 900);
+  setTimeout(function () {
+    text.innerHTML = "";
+  }, 1000);
+}, 5400);
+
+setInterval(function () {
+    
+  //blogger
+
+  setTimeout(function () {
+    text.append("b");
+  }, 100);
+  setTimeout(function () {
+    text.append("l");
+  }, 200);
+  setTimeout(function () {
+    text.append("o");
+  }, 300);
+  setTimeout(function () {
+    text.append("g");
+  }, 400);
+  setTimeout(function () {
+    text.append("g");
+  }, 500);
+  setTimeout(function () {
+    text.append("e");
+  }, 600);
+  setTimeout(function () {
+    text.append("r");
+    text.style.paddingRight = "5px"
+  }, 700);
+
+  setTimeout(function () {
+    setTimeout(function () {
+      text.style.padding = "0"
+      text.innerHTML = "blogge";
+    }, 100);
+    setTimeout(function () {
+      text.innerHTML = "blogg";
+    }, 200);
+    setTimeout(function () {
+      text.innerHTML = "blog";
+    }, 300);
+    setTimeout(function () {
+      text.innerHTML = "blo";
+    }, 400);
+    setTimeout(function () {
+      text.innerHTML = "bl";
+    }, 500);
+    setTimeout(function () {
+      text.innerHTML = "b";
+    }, 600);
+    setTimeout(function () {
+      text.innerHTML = "";
+    }, 700);
+  }, 1000);
+
+  //youtuber
+
+  setTimeout(function () {
+    setTimeout(function () {
+      text.append("y");
+    }, 100);
+    setTimeout(function () {
+      text.append("o");
+    }, 200);
+    setTimeout(function () {
+      text.append("u");
+    }, 300);
+    setTimeout(function () {
+      text.append("t");
+    }, 400);
+    setTimeout(function () {
+      text.append("u");
+    }, 500);
+    setTimeout(function () {
+      text.append("b");
+    }, 600);
+    setTimeout(function () {
+      text.append("e");
+    }, 700);
+    setTimeout(function () {
+      text.append("r");
+      text.style.paddingRight = "5px"
+    }, 700);
+  }, 2000);
+
+  setTimeout(function () {
+    setTimeout(function () {
+      text.style.padding = "0"
+      text.innerHTML = "youtube";
+    }, 100);
+    setTimeout(function () {
+      text.innerHTML = "youtub";
+    }, 200);
+    setTimeout(function () {
+      text.innerHTML = "youtu";
+    }, 300);
+    setTimeout(function () {
+      text.innerHTML = "yout";
+    }, 400);
+    setTimeout(function () {
+      text.innerHTML = "you";
+    }, 500);
+    setTimeout(function () {
+      text.innerHTML = "yo";
+    }, 600);
+    setTimeout(function () {
+      text.innerHTML = "y";
+    }, 700);
+    setTimeout(function () {
+      text.innerHTML = "";
+    }, 800);
+  }, 3000);
+
+  // freelancer
+
+  setTimeout(function () {
+    setTimeout(function () {
+      text.append("f");
+    }, 100);
+    setTimeout(function () {
+      text.append("r");
+    }, 200);
+    setTimeout(function () {
+      text.append("e");
+    }, 300);
+    setTimeout(function () {
+      text.append("e");
+    }, 400);
+    setTimeout(function () {
+      text.append("l");
+    }, 500);
+    setTimeout(function () {
+      text.append("a");
+    }, 600);
+    setTimeout(function () {
+      text.append("n");
+    }, 700);
+    setTimeout(function () {
+      text.append("c");
+    }, 800);
+    setTimeout(function () {
+      text.append("e");
+    }, 900);
+    setTimeout(function () {
+      text.append("r");
+      text.style.paddingRight = "5px"
+    }, 1000);
+  }, 4100);
+
+  setTimeout(function () {
+    setTimeout(function () {
+      text.style.padding = "0"
+      text.innerHTML = "freelance";
+    }, 100);
+    setTimeout(function () {
+      text.innerHTML = "freelanc";
+    }, 200);
+    setTimeout(function () {
+      text.innerHTML = "freelan";
+    }, 300);
+    setTimeout(function () {
+      text.innerHTML = "freela";
+    }, 400);
+    setTimeout(function () {
+      text.innerHTML = "freel";
+    }, 500);
+    setTimeout(function () {
+      text.innerHTML = "free";
+    }, 600);
+    setTimeout(function () {
+      text.innerHTML = "fre";
+    }, 700);
+    setTimeout(function () {
+      text.innerHTML = "fr";
+    }, 800);
+    setTimeout(function () {
+      text.innerHTML = "f";
+    }, 900);
+    setTimeout(function () {
+      text.innerHTML = "";
+    }, 1000);
+  }, 5400);
+}, 6700);
